4acf74b1c35646a49d926768a5d373c25924c8a5
Gerald Yeo <gerald.yeo@gmail.com>
2019-05-31 15:39:23 +0800

    wip(site): templates

--- clients/foostack-dev-site/gatsby-node.js
+++ clients/foostack-dev-site/gatsby-node.js
@@ -3,5 +3,37 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const { get } = require('lodash');
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+async function getAllBlogPost(createPage, graphql) {
+  const query = `
+    {
+      allContentfulBlogPost(limit: 10) {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `;
+  const blogArticleTemplate = path.resolve('src/templates/blog-article.js');
+  const allBlogPostsResult = await graphql(query);
+  const allBlogPostEdges = get(allBlogPostsResult, 'data.allContentfulBlogPost.edges', []);
+  allBlogPostEdges.forEach(edge => {
+    createPage({
+      path: `/articles/${edge.node.id}`,
+      component: blogArticleTemplate,
+      // The context is passed as props to the component as well
+      // as into the component's GraphQL query.
+      context: {
+        id: edge.node.id,
+      },
+    });
+  });
+}
+
+exports.createPages = async ({ actions: { createPage }, graphql }) => {
+  await getAllBlogPost(createPage, graphql);
+};
--- clients/foostack-dev-site/src/layouts/default.js
+++ clients/foostack-dev-site/src/layouts/default.js
@@ -1,18 +1,10 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 import { GlobalStyles, Provider, Box } from '@foostack-dev/restyled';
+import Header from '../components/header';
 
-import Header from './header';
-
-const Layout = ({ children }) => {
+const DefaultLayout = ({ children }) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -43,8 +35,8 @@ const Layout = ({ children }) => {
   );
 };
 
-Layout.propTypes = {
+DefaultLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-export default Layout;
+export default DefaultLayout;
