d75bd846193bd773e19306abedb1117125c92dea
Gerald Yeo <gerald.yeo@gmail.com>
2019-06-07 10:50:30 +0800

    refactor(restyled): migrate to v5 styled-system

--- packages/restyled/src/components/layouts/box.js
+++ packages/restyled/src/components/layouts/box.js
@@ -1,29 +1,32 @@
 import styled from 'styled-components';
-import { alignSelf, color, display, flex, fontSize, order, space, width } from 'styled-system';
+import border from '@styled-system/border';
+import color from '@styled-system/color';
+import layout from '@styled-system/layout';
+import shadow from '@styled-system/shadow';
+import space from '@styled-system/space';
+import typography from '@styled-system/typography';
+import propTypes from '@styled-system/prop-types';
 
-export const Box = styled.div`
-  box-sizing: border-box;
-  ${alignSelf}
-  ${color}
-  ${display}
-  ${flex}
-  ${fontSize}
-  ${order}
-  ${space}
-  ${width}
-`;
+export const Box = styled.div(
+  { boxSizing: 'border-box' },
+  border,
+  color,
+  layout,
+  shadow,
+  space,
+  typography,
+);
 
 Box.displayName = 'Restyled.Box';
 
 Box.propTypes = {
-  ...alignSelf.propTypes,
-  ...color.propTypes,
-  ...display.propTypes,
-  ...flex.propTypes,
-  ...fontSize.propTypes,
-  ...order.propTypes,
-  ...space.propTypes,
-  ...width.propTypes,
+  ...propTypes.border,
+  ...propTypes.color,
+  ...propTypes.flexbox,
+  ...propTypes.layout,
+  ...propTypes.shadow,
+  ...propTypes.space,
+  ...propTypes.typography,
 };
 
 export default Box;
